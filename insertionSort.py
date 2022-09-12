7a594704e220fbc4eee3ed5f99359454a4431a6c
AmadIrfan <amadirfan443@gmail.com>
2022-09-12 14:12:15 +0500

    python

--- insertionSort.py
+++ insertionSort.py
@@ -8,6 +8,9 @@ for i in range(1,len(A)): #
     a=A[j]
     while j>=0 and A[j]>key:
         A[j+1]=A[j]
+        A[j+1]=A[j]
+        A[j+1]=A[j]
+        A[j+1]=A[j]
         j=j-1
         A[j+1]=key
 print(A)
