ccf69dc3344b50c5f3ea1382e5e73b496b80f51e
AmadIrfan <amadirfan443@gmail.com>
2022-09-12 14:11:32 +0500

    first commit

---
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -0,0 +1,6 @@
+dict ={'amad':'2021-20-21','saad':'2021-21-21','jawad':'2202-12-11','ahmad':'4323-42-42','abdullah':'1231-23-23'}
+Value=input('Enter any value ')
+print(dict[Value])
+
+#for obj in dict:
+    #print
