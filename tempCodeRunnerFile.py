27dc2bd50384789aa195a81d3dcb244ff052335e
Amad Irfan <99482198+AmadIrfan@users.noreply.github.com>
2022-11-27 19:43:30 +0500

    Add files via upload

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,9 @@
-k
+
+# def viruse():
+#     for i in range(0,10):
+#         file=open('user/dell//ibn{}.txt'.format(i),"w")
+#         file.write(' i love you Amna baby')
+#         file.close()
+
+# if __name__=="__main__":
+#     viruse()
